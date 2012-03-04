240db35271675e28784f0a49a41a2c5c203bc739
Moritz Peters <maritz.peters@gmail.com>
2012-03-04 23:46:40 +0100

    Refactored test self channel to use psubscribe instead of normal subscribe

--- socket_controllers/SelfSocketChannel.js
+++ socket_controllers/SelfSocketChannel.js
@@ -1,36 +1,45 @@
 var registry = require(__dirname+'/../registry.js');
 
 
-var checkMessages = function (socket, name, amount) {
-  registry.redis.lpop('messages:'+name, function (err, msg) {
+var checkMessages = function (socket, id, amount) {
+  registry.redis.lpop('messages:'+id, function (err, msg) {
     if (err) {
       console.log('Error in SelfSocketChannel', err);
     } else {
       if (msg !== null) {
+        console.log('emitting', id, socket.handshake.session.userdata.name, socket.handshake.session_id);
         socket.emit('new_msg', {
           msg: msg
         });
         if (--amount > 0) {
-          checkMessages(socket, name, amount);
+          checkMessages(socket, id, amount);
         }
       }
     }
   });
 }
 
+registry.redis_sub.psubscribe('messages:*');
+
+var sockets = {};
+
+registry.redis_sub.on('pmessage', function (pattern, channel, amount) {
+  var id = channel.substr(channel.lastIndexOf(':')+1);
+  console.log(channel, amount, id, sockets);
+  if (sockets.hasOwnProperty(id)) {
+    checkMessages(sockets[id], id, amount);
+  }
+});
+
 exports.connectionHandler = function (socket) {
-  console.log('A logged in user "'+socket.handshake.session.userdata.name+'" connected to the self socket.');
+  //console.log('A logged in user "'+socket.handshake.session.userdata.name+'" connected to the self socket.');
 
-  var name = socket.handshake.session.userdata.name;
-
-  registry.redis_sub.subscribe('messages:'+name);
-  
-  registry.redis_sub.on('message', function (channel, amount) {
-    checkMessages(socket, name, amount);
-  });
+  var id = socket.handshake.session.userdata.id;
+  sockets[id] = socket;
   
   socket.on('disconnect', function () {
-    console.log('BYE BYE', socket.handshake.session.userdata.name);
+    //console.log('BYE BYE', id);
+    delete sockets[id];
   });
 };
 
