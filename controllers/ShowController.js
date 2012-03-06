30801683881aaeb958f3c5f24b55ac3d75ea683d
Moritz Peters <maritz.peters@gmail.com>
2012-03-06 23:19:40 +0100

    Clean up user stuff from ShowController

--- controllers/ShowController.js
+++ controllers/ShowController.js
@@ -22,17 +22,17 @@ ShowError.prototype.__proto__ = Error.prototype;
 app.get('/', auth.isLoggedIn, auth.may('list', 'Show'), function (req, res, next) {
   Show.find(function (err, ids) {
     if (err) {
-      next(new ShowError('Fetching the user ids failed: '+err));
+      next(new ShowError('Fetching the ids failed: '+err));
     } else {
       async.map(ids, function loadById(id, callback) {
         var show = new Show(id, function (err) {
           callback(err, show.allProperties());
         });
-      }, function doneLoading(err, users) {
+      }, function doneLoading(err, shows) {
         if (err) {
-          next(new ShowError('Fetching the userlist failed: '+err));
+          next(new ShowError('Fetching the list failed: '+err));
         } else {
-          res.ok(users);
+          res.ok(shows);
         }
       });
     }
@@ -45,155 +45,47 @@ app.get('/:id([0-9]+)', auth.isLoggedIn, loadModel('Show'), function (req, res) 
 
 
 function store (req, res, next) {
-  var user = req.loaded['Show'];
+  var show = req.loaded['Show'];
   var data = {
-    name: req.param('name'),
-    password: req.param('password'),
-    email: req.param('email')
+    name: req.param('name')
   };
-  user.store(data, function (err) {
+  show.p(data, function (err) {
     if ( ! err) {
       next();
     } else {
-      next(new ShowError({error: err, fields: user.errors}, 400));
+      next(new ShowError({error: err, fields: show.errors}, 400));
     }
   });
 }
 
-function newUser (req, res, next) {
+function newShow (req, res, next) {
   req.loaded = {
     Show: new Show()
   };
   next();
 }
 
-function sendSessionUserdata(req, res) {
-  res.ok(req.session.userdata);
-}
+app.post('/', auth.may('create', 'Show'), newShow, store);
 
-function updateSession (req, res, next) {
-  if (req.loaded['Show'] && req.loaded['Show'] instanceof User && req.loaded['Show'].__inDB) {
-    req.session.logged_in = true;
-    req.session.userdata = req.loaded['Show'].allProperties();
-    next();
-  } else {
-    next(new ShowError('Can\'t set session to req.loaded_user because it\'s not a valid and loaded nohm model.'));
-  }  
-}
-
-app.post('/', auth.may('create', 'User'), newUser, store, updateSession, sendSessionUserdata);
-
-app.put('/:id([0-9]+)', auth.isLoggedIn, auth.may('edit', 'User'), loadShow, store, updateSession, sendSessionUserdata);
-
-if (process.env.NODE_ENV !== 'production') {
-  app.get('/:takeOrGive(take|give)/me/admin', auth.isLoggedIn, function (req, res, next) {
-    var admin = req.param('takeOrGive') === 'give';
-    req.user.p('admin', admin);
-    req.user.save(function (err) {
-      if (err) {
-        next(new Error(err));
-      } else {
-        res.ok(req.user.allProperties());
-      }
-    });
-  });
-}
-
-app.put('/:allowOrDeny(allow|deny)/:id([0-9]+)', auth.isLoggedIn, auth.may('allow', 'User'), loadShow, function (req, res, next) {
-  var allowOrDeny = req.param('allowOrDeny');
-  var action = req.param('action');
-  var subject = req.param('subject');
-  if (action && subject) {
-    var new_acl = req.loaded['Show'][allowOrDeny](action, subject);
-    req.loaded['Show'].save(function (err) {
-      if (err) {
-        next(new Error(err));
-      } else {
-        res.ok(new_acl);
-      };
-    });
-  } else {
-    next(new ShowError('Invalid parameters'));
-  }
-});
+app.put('/:id([0-9]+)', auth.isLoggedIn, auth.may('edit', 'Show'), loadModel('Show'), store);
 
 app.get('/checkName', function (req, res, next) {
-  var name = req.param('name');
-  if (name) {
-    if (name === req.user.p('name')) {
-      res.ok('That is your name.');
-    } else {
-      setTimeout(function () {
-        User.find({name: name}, function (err, ids) {
-          if (err) {
-            next(new ShowError('Database error: '+err, 500));
-          } else if (ids.length > 0) {
-            next(new ShowError('Name taken.', 400));
-          } else {
-              res.ok();
-          }
-        });
-      }, 800);
-    }
-  } else {
-    next(new ShowError('No name to check in parameters.'));
-  }
+  
 });
 
-function login (req, res, next) {
-  // TODO: needs login-per-ip counter and ban.
-
-  var user = new User();
-  var name = req.param('name') || false;
-  var password = req.param('password') || false;
-  user.login(name, password, function (success) {
-    if (success) {
-      req.loaded['Show'] = user;
-      next();
-    } else {
-      setTimeout(function () { // artificial delay to make bruteforcing less practical
-        logout(req);
-        next(new ShowError({error: 'Wrong authentication.', fields: {general: ['wrong_login']}}, 400));
-      }, 400);
-    }
-  });
-}
-
-app.post('/login', login, updateSession, sendSessionUserdata);
-
-app.get('/getLoginData', auth.isLoggedIn, sendSessionUserdata);
-
-function logout (req) {
-  req.session.userdata = {};
-  req.session.logged_in = false; 
-}
-
-app.get('/logout', function (req, res) {
-  logout(req);
-  res.ok();
-});
-
-app.del('/:id([0-9]+)', auth.isLoggedIn, auth.may('delete', 'User'), loadShow, function (req, res, next) {
-  var doLogout = false;
-  if (req.session.userdata.id === req.loaded['Show'].id) {
-    doLogout = true;
-  }
+app.del('/:id([0-9]+)', auth.isLoggedIn, auth.may('delete', 'Show'), loadModel('Show'), function (req, res, next) {
   req.loaded['Show'].remove(function (err) {
     if (err) {
       next(new ShowError('Delete failed: '+err, 500));
     } else {
-      if (doLogout) {
-        logout(req, res);
-      } else {
-        res.ok();
-      }
+      res.ok();
     }
   });
 });
 
 
 app.mounted(function (parent){
-  console.log('mounted User REST controller');
+  console.log('mounted Show REST controller');
 });
 
 module.exports = app;
